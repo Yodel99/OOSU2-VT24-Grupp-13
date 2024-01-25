adff526e0c25357930dabbaf1bdd61e7f160b4cc
Joel Ahlström <joelahlstrom@outlook.com>
2024-01-25 13:44:44 +0100

    Veriation 1.0 av programmet, alla ska utgå ifrån denna.

---
+++ PatienthanteringPL/PatientHantering.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatienthanteringPL
+{
+    public partial class PatientHantering : Form
+    {
+        public PatientHantering()
+        {
+            InitializeComponent();
+        }
+    }
+}
