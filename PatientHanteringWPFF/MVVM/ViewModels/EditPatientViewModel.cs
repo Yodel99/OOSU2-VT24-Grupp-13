ac0005aac686bbab0414da12fd7b015f7c5c2df9
Yodel99 <joelahlstrom@outlook.com>
2024-03-08 14:03:25 +0100

    Mina delar av mitt frösta usecase

--- PatientHanteringWPFF/MVVM/ViewModels/EditPatientViewModel.cs
+++ PatientHanteringWPFF/MVVM/ViewModels/EditPatientViewModel.cs
@@ -1,33 +1,69 @@
-﻿using EnityLayer;
+﻿using DataLayer;
+using EnityLayer;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using PatientHanteringWPFF.Core;
 using ServiceLayer;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace PatientHanteringWPFF.MVVM.ViewModels
 {
     public class EditPatientViewModel : ObservableObject
     {
         private GetListsController getListsController;
-
-        private List<Patient> patients = null;
-        public List<Patient> Patients
+        private UpdatePatientController updatePatientController;
+        private Patient chosenPatientNr;
+        public Patient ChosenPatientNr
         {
-            get => patients;
-            set { patients = (List<Patient>)getListsController.GetPatients(); }
-
+            get { return chosenPatientNr; }
+            set
+            {
+                chosenPatientNr = value;
+                OnPropertyChanged(nameof(ChosenPatientNr));
+            }
         }
-
         
         public EditPatientViewModel()
         {
             getListsController = new GetListsController();
-            Patients = new List<Patient>();
+            updatePatientController = new UpdatePatientController();   
+            Patients = new ObservableCollection<Patient>(getListsController.GetPatients());
         }
+
+        private string selectedAttribute;
+        public string SelectedAttribute
+        {
+            get { return selectedAttribute; }
+            set
+            {
+                selectedAttribute = value;
+                OnPropertyChanged(nameof(SelectedAttribute));
+            }
+        }
+
+
+
+        private string chosenInput;
+        public string ChosenInput
+        {
+            get { return chosenInput; }
+            set
+            {
+                chosenInput = value;
+                OnPropertyChanged(nameof(ChosenInput));
+            }
+        }
+        
+        
+        
 
 
 
@@ -37,7 +73,43 @@ namespace PatientHanteringWPFF.MVVM.ViewModels
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        public RelayCommand RefreshCommand => new RelayCommand(_execute => RefreshData());
 
+        private void RefreshData()
+        {
+            //Patients.Clear();
+            //foreach (Patient patient in getListsController.GetPatients())
+            //{
+            //    Patients.Add(patient);
+            //}
+            Patients.Clear();
+            Patients = new ObservableCollection<Patient>(getListsController.GetPatients());
+        }
 
+        private ObservableCollection<Patient> patients = null;
+        public ObservableCollection<Patient> Patients
+        {
+            get { return patients; }
+            set
+            {
+                patients = value;
+                OnPropertyChanged(nameof(Patients));
+            }
+
+        }
+
+        #region SaveCommand
+        public RelayCommand SaveCommand => new RelayCommand(_execute => SaveInput(ChosenPatientNr.PatientNr, SelectedAttribute, ChosenInput));
+        private void SaveInput(string chosenPatientNr, string chosenAttribute, string chosenInput)
+        {
+            Patient patient = new Patient();
+            patient.PatientNr = chosenPatientNr;
+            updatePatientController.UpdatePatientInfo(chosenPatientNr, chosenAttribute, chosenInput);
+
+            RefreshData();
+            
+            MessageBox.Show($"Patient: {patient.PatientNr} {chosenAttribute} has been succesfully updated with your chosen input: {ChosenInput}");
+        }
+        #endregion
     }
 }
